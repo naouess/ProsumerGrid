053004f605ccbe3e4f5ae9e9914ed20219e71f69
unknown <nesrine.ouanes@hotmail.com>
2020-04-11 09:15:47 +0200

    Adjusted code

--- src/nodes/GenericNode.jl
+++ src/nodes/GenericNode.jl
@@ -1,28 +1,17 @@
 """Abstract type for all nodes"""
 abstract type AbstractNode end
 
-# ? is this additional specification of types needed ?
-# ? Maybe reconsider naming of "Bus" and "Component" ?
-# Idea behind this: the differentiation is mainly due to the fact that components and buses have different
-# equations. So more efficient to seperate both types and have this integrated in
-# the type hierarchy.
 """Abstract subtypes. A node can be a bus or a component"""
 abstract type AbstractBus <: AbstractNode end
 abstract type AbstractComponent <: AbstractNode end
 
 ###
-# Here import Base packages needed for creating macros
 using Parameters
 # using Base.Meta
 # using MacroTools
 # ...
 
-# ? or immutable struct ?
-# ? @with_kw in what way does this differ from just accessing the fields by .field_name ?
-# ? Use parametric struct to differentiate if some parameters are passed as constants (single values)
-# or as time series ?
-# Parametric struct => T depends ond whether inputs are constants or time series
-mutable struct GenericComponent{T} <: AbstractComponent
+struct GenericComponent <: AbstractComponent
     C		# Storage capacity
     η_load	# Load conversion efficiency
     η_gen	# Generation efficiency
@@ -48,11 +37,8 @@ end
 
 function main_equation_component(component <: AbstractComponent)
 	# Definition of main equation here
-	# ? dl needs broadcast ?
 	@. component.dl = (# Write rhs of equation here
 						) / component.C
-
-	# use quote end ?
 	quote
 
 	end
@@ -72,14 +58,8 @@ function bounds(var, bounds)
 	# ? How to integrate this ? CallBack-Function ? or using JumP ?
 end
 
-function check_input()
-	# Here more constraints on input validity check
-	#
-end
+### Try with Metaprogramming
 
-### Try with Metaprogramming (using macro)
-
-# The following is used to declare a struct at parsing time (=> Metaprogramming).
 parameters = [
 	:C,			# Storage capacity
 	:η_load,	# Load conversion efficiency
@@ -99,23 +79,12 @@ function buildstruct_component(name, parameters)
 	struct_def = Expr(
         :struct, false, # why is this second argument needed?
         :($name <: AbstractComponent),
-        Expr(:block, parameters..., # set all the parmeters as fields in the struct
-            Expr(:(=), # define the constructor
+        Expr(:block, parameters...,
+            Expr(:(=), # inner constructor
                 Expr(:call, name, Expr(:parameters, parameters... )),
                 Expr(:call, :new,  parameters...))
     )
-	# This function returns a piece of code that generate an expression
-	# Still unclear how to evaluate this to create the struct at runtime, so that
-	# typeof(struct_def) is DataType (CompositeType)
-
-	# ? How to define an inner consructor using quoting => see above! :)
 end
-
-# Example of struct definition
-name = :Battery
-parameters = [:a, :b]
-Battery = buildstruct_component(name, parameters)
-typeof(Battery) # Type is Expr => still not evaluated
 
 macro PowerNode(name, parameters)
     # Defnitions
