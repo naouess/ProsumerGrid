3cd9ce22e8b61bb5ce1d7d6651b7650575084b22
naouess <n.ouanes@posteo.de>
2020-05-28 11:35:31 +0200

    modified battery node definition

--- src/nodes/NodeTypesPD.jl
+++ src/nodes/NodeTypesPD.jl
@@ -2,10 +2,11 @@
 TODO:
 [̌] Check where a broadcast is necessary (parameter and variable types)
 [x] Define MassMatrix correctly
+[] Recheck definition of battery node
 [] Test definition of nodes, once the power flow equation is integrated
 =#
 
-using PowerDynamics
+#using PowerDynamics
 
 # The following import lines are needed to avoid some LoadErrors: there seems to be a
 # more proper way to define custom nodes without having to use these imports:
@@ -46,20 +47,18 @@ end [[ω, dω]] begin
     dω = (- P.load - F) / M
 end
 
-@DynamicNode Battery(ξ, η, u, M, C) begin
+# This definition needs recheck.
+@DynamicNode Battery(η, M, C) begin
     MassMatrix(; m_u = false, m_int = [true, true])
 end begin
-    w = ξ - (u.LI + u.ILC)
-    @assert ξ * w >= 0 "ξ and w must have the same sign, according to the defined sign convention"
-    @assert abs(ξ) - abs(w) >= 0 "ξ always bigger than w"
     @assert η in [0.0, 1.0]
     P.load = []
     P.gen = []
 end [[ω, dω], [l, dl]] begin
     # ? Differentiate between charging and discharging states of battery through a
-    # conditional on the sign of F?
-    P.gen = (u.LI + u.ILC) * η.gen
-    P.load = (u.LI + u.ILC) / η.load
+    # conditional on the sign of F? (both states don't occur simultaneously)
+    # P.gen = (u.LI + u.ILC) * η.gen
+    # P.load = (u.LI + u.ILC) / η.load
     # ? Can du be defined like this?
     du = 0
     dl = (η.load * P.load - P.gen / η.gen) / C
