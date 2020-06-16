398dbb22d67841b401fd20ccc0656560e79d227e
naouess <n.ouanes@posteo.de>
2020-06-16 15:22:56 +0200

    Adjusted to rectify type errors.

--- src/nodes/NodeTypesPD.jl
+++ src/nodes/NodeTypesPD.jl
@@ -1,60 +1,76 @@
-#=
-TODO:
-[̌] Check where a broadcast is necessary (parameter and variable types)
-[x] Define MassMatrix correctly
-[] Recheck definition of battery node
-[] Test definition of nodes, once the power flow equation is integrated
-=#
+using PowerDynamics
 
-#using PowerDynamics
 
 # The following import lines are needed to avoid some LoadErrors: there seems to be a
 # more proper way to define custom nodes without having to use these imports:
-# Follow up on this (see thread here: https://github.com/JuliaEnergy/PowerDynamics.jl/issues/71)
+# TODO: Follow up on this (see thread here: https://github.com/JuliaEnergy/PowerDynamics.jl/issues/71)
 import Base: @__doc__
-import PowerDynamics: AbstractNode, showdefinition, AbstractLine
+import PowerDynamics: AbstractNode, showdefinition, AbstractLine, construct_vertex
 
-@DynamicNode PV(ξ, η, u, M) begin
-    MassMatrix(; m_u = false, m_int = [true])
+# TODO: Check where a broadcast is necessary (parameter and variable types)
+
+@DynamicNode PV(ξ, η_gen, LI, ILC, M) begin
+    MassMatrix(; m_u = false, m_int = [true, true])
 end begin
     # [all prepratory things that need to be run just once]
-    w = ξ - (u.LI + u.ILC)
-    @assert ξ * w >= 0 "ξ and w must have the same sign, according to the defined sign convention"
-    @assert abs(ξ) - abs(w) >= 0 "ξ always bigger than w"
-    @assert η in [0.0, 1.0]
+    w = ξ - (LI + ILC)
+    #@assert ξ * w >= 0 "ξ and w must have the same sign, according to the defined sign convention"
+    #@assert abs(ξ) - abs(w) >= 0 "ξ always bigger than w"
+    #@assert η in [0.0, 1.0]
     # define P as struct?
-    P.gen = []
-end [[ω, dω]] begin
+    P_gen = 0
+end [[ϕ, dϕ], [ω, dω]] begin
     # [the actual dynamics equation]
     # [important to set the output variables]
-    du = P.gen - (u.LI + u.ILC) * η.gen
+    du = P_gen - (LI + ILC) * η_gen
     F = i
-    dω = (P.gen - F) / M
+    dϕ = ω
+    dω = (P_gen - F) / M
 end
 
-@DynamicNode Load(ξ, η, u, M) begin
-    MassMatrix(; m_u = false, m_int = [true])
+@DynamicNode Slack(ξ, η_gen, LI, ILC, M) begin
+    MassMatrix(; m_u = false, m_int = [true, true])
 end begin
-    w = ξ - (u.LI + u.ILC)
-    @assert ξ * w >= 0 "ξ and w must have the same sign, according to the defined sign convention"
-    @assert abs(ξ) - abs(w) >= 0 "ξ always bigger than w"
-    @assert η in [0.0, 1.0]
+    # [all prepratory things that need to be run just once]
+    w = ξ - (LI + ILC)
+    #@assert ξ * w >= 0 "ξ and w must have the same sign, according to the defined sign convention"
+    #@assert abs(ξ) - abs(w) >= 0 "ξ always bigger than w"
+    #@assert η in [0.0, 1.0]
     # define P as struct?
-    P.load = []
-end [[ω, dω]] begin
-    du = P.load - (u.LI + u.ILC) / η.load
-    F = i # since i is the variable for the flows as defined in NodeMacro.jl
-    dω = (- P.load - F) / M
+    P_gen = 0
+end [[ϕ, dϕ], [ω, dω]] begin
+    # [the actual dynamics equation]
+    # [important to set the output variables]
+    du = P_gen - (LI + ILC) * η_gen
+    F = i
+    dϕ = ω
+    dω = (P_gen - F) / M
 end
 
-# This definition needs recheck.
+@DynamicNode Load(ξ, η_load, LI, ILC, M) begin
+    MassMatrix(; m_u = false, m_int = [true, true])
+end begin
+    w = ξ - (LI + ILC)
+    #@assert ξ * w >= 0 "ξ and w must have the same sign, according to the defined sign convention"
+    #@assert abs(ξ) - abs(w) >= 0 "ξ always bigger than w"
+    #@assert η in [0.0, 1.0]
+    # define P as struct?
+    P_load = 0
+end [[ϕ, dϕ], [ω, dω]] begin
+    du = P_load - (LI + ILC) / η_load
+    F = i # since i is the variable for the flows as defined in NodeMacro.jl
+    dϕ = ω
+    dω = (- P_load - F) / M
+end
+
+# TODO: Recheck definition of battery node
 @DynamicNode Battery(η, M, C) begin
     MassMatrix(; m_u = false, m_int = [true, true])
 end begin
     @assert η in [0.0, 1.0]
     P.load = []
     P.gen = []
-end [[ω, dω], [l, dl]] begin
+end [[ϕ, dϕ], [ω, dω]] begin
     # ? Differentiate between charging and discharging states of battery through a
     # conditional on the sign of F? (both states don't occur simultaneously)
     # P.gen = (u.LI + u.ILC) * η.gen
@@ -63,5 +79,6 @@ end [[ω, dω], [l, dl]] begin
     du = 0
     dl = (η.load * P.load - P.gen / η.gen) / C
     F = i # since i is the variable for the flows as defined in NodeMacro.jl
+    dϕ = ω
     dω = (P.gen - P.load - F) / M
 end
