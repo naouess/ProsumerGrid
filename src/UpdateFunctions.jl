a52152e9e1b7954309b6572d789673f20e25900c
naouess <n.ouanes@posteo.de>
2020-08-18 00:06:11 +0200

    Added comments and removed unneeded lines

--- src/UpdateFunctions.jl
+++ src/UpdateFunctions.jl
@@ -14,11 +14,9 @@ PeriodicCallback function acting on the `integrator` that is called every simula
 """
 function (hu::HourlyUpdate)(integrator)
 	# The current hour is extracted using the following line:
-	println("The time is currently ", integrator.t)
 	hour = mod(round(Int, integrator.t/3600.), 24) + 1
 	# getting the last hour of the day
 	last_hour = mod(hour-2, 24) + 1
-	# println("The hour is ", last_hour)
 
 	# the following lines: last hour letzte Stunde auf Integral der Stunde davor setzen
 	for j = 1:N
@@ -26,7 +24,8 @@ function (hu::HourlyUpdate)(integrator)
 		# println(integrator.p[1][j].mismatch_yesterday[last_hour])
 		integrator.u[5*j-1] = 0.
 		integrator.p[1][j].current_background_power = integrator.p[1][j].daily_background_power[hour]
-		println("I'm doing this at time ", integrator.t, " for node ", j)
+		# println("The daily background power for node ", j, " : ", integrator.p[1][j].daily_background_power[hour])
+		# println("I'm doing this at time ", integrator.t, " for node ", j)
 	end
 	# Why is the abs. value needed ?
 	# integrator.u[power_abs_idx] .= 0.
@@ -37,7 +36,7 @@ function DailyUpdate(integrator)
 	for j = 1:N
 		integrator.p[1][j].daily_background_power = integrator.p[1][j].Q * (integrator.p[1][j].daily_background_power
 		+ integrator.p[1][j].kappa * integrator.p[1][j].mismatch_yesterday)
-		println("And also this at time ", integrator.t, " for node ", j)
+		# println("And also this at time ", integrator.t, " for node ", j)
 	end
 	nothing
 end
