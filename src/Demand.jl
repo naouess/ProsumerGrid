8cef9dcda651bcb7c25d4aa934e3520cbcff7f72
naouess <n.ouanes@posteo.de>
2020-08-18 00:47:31 +0200

    Corrected demand function

--- src/Demand.jl
+++ src/Demand.jl
@@ -2,12 +2,12 @@ using Interpolations
 using JLD2, FileIO, GraphIO, CSV, DataFrames
 
 begin
-	# num_days = 20
-	# l_day = 24*3600
-	# l_hour = 3600
-	# N = 4
+	num_days = 35
+	l_day = 24*3600
+	l_hour = 3600
+	N = 4
 end
-# Generating demand function as in sim_demand_learning.jl
+
 begin
 	struct demand_amp_var
 		demand
@@ -18,7 +18,8 @@ begin
 		dav.demand[index + 1,:]
 	end
 
-	# Real demand data:
+	## Generating demand function as in sim_demand_learning.jl
+
 	dem_data = CSV.read("$dir/profil.csv")
 
 	dem_data_week = CSV.read("$dir/profil_week.csv")
@@ -37,7 +38,8 @@ begin
 	inter = interpolate([10. ./100 * randn(N) for i in 1:(num_days * samples + 1)], BSpline(Linear()))
 	residual_demand = t -> inter(1. + t / (24*3600) * samples) # 1. + is needed to avoid trying to access out of range
 
-    # Synthetic demand function:
+    ## Generating synthetic demand function:
+
 	# slowly increasing and decreasing amplitude - only working for <= 20 days now
 	# demand_amp1 = demand_amp_var(repeat([80 80 80 10 10 10 40 40 40 40 40], outer=Int(N/4))') # random positive amp over days by 10%
 	# demand_amp2 = demand_amp_var(repeat([10 10 10 80 80 80 40 40 40 40 40], outer=Int(N/4))') # random positive amp over days by 10%
@@ -50,4 +52,4 @@ begin
 	# inter = interpolate([.2 * randn(N) for i in 1:(num_days * samples + 1)], BSpline(Linear()))
 	# residual_demand = t -> inter(1. + t / (24*3600) * samples) # 1. + is needed to avoid trying to access out of range
 end
-# plot(1:100000, t -> periodic_demand(t)[1]+residual_demand(t)[1])
+# plot(1:24*3600, t -> periodic_demand(t)[1] + residual_demand(t)[1])
