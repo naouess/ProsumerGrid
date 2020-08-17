bd4f5e539db604903b76da40b1ae22d5362d6b5a
naouess <n.ouanes@posteo.de>
2020-08-18 00:05:04 +0200

    Adjusted StaticEdge and helper function definitions

--- src/PowerLine.jl
+++ src/PowerLine.jl
@@ -17,23 +17,23 @@ begin
             destination_ϕ = v_d[1]
             # e = K * sin(destination_ϕ - source_ϕ)
             # Linearized model:
-            e .= K .* (destination_ϕ .- source_ϕ)
+            F = K .* (destination_ϕ .- source_ϕ)
+            e .= [K, destination_ϕ, source_ϕ, F]
             nothing
         end
-        return StaticEdge(f! = rhs!, dim = 1)
+        return StaticEdge(f! = rhs!, dim = 4)
     end
 end
 
 ### Helper function
-function total_flow(e_s, e_d)
+function total_flow(x, e_s, e_d)
     # Keeping with the convention of negative sign for outging flow
     net_flow = 0.
     for e in e_s
-        net_flow -= e[1]
+        net_flow -= e[1] * (x[1] - e[2])
     end
     for e in e_d
-        net_flow += e[1]
+        net_flow += e[1] * (e[3] - x[1])
     end
-    # println("This is the net flow: ", net_flow)
     return net_flow
 end
