7a1ea420f56d38f690f89212e0a2fa17a4c549aa
naouess <n.ouanes@posteo.de>
2020-08-17 13:15:39 +0200

    Added helper lines for debugging

--- src/PowerNodes.jl
+++ src/PowerNodes.jl
@@ -10,138 +10,132 @@ struct PV <: AbstractNode
         ξ
         η_gen
         LI
-        M
+        M_inv
 end
-PV(; ξ, η_gen, LI, M) = PV(ξ, η_gen, LI, M)
+PV(; ξ, η_gen, LI, M_inv) = PV(ξ, η_gen, LI, M_inv)
 function construct_vertex(par::PV)
     ξ = par.ξ
     η_gen = par.η_gen
     K_P = par.LI.kp
 	K_I = par.LI.ki
-	T = par.LI.T
-    M = par.M
+	T_inv = par.LI.T_inv
+    M_inv = par.M_inv
 
     function rhs!(dx, x, e_s, e_d, p, t)
-        ϕ = x[1]
-        ω = x[2]
-		χ = x[3]
-		integrated_LI = x[4]
-		integrated_w = x[5]
+		ϕ = @view x[1]
+		ω = @view x[2]
+		χ = @view x[3]
+		integrated_LI = @view x[4]
+		integrated_w = @view x[5]
 
-		u_LI = - K_P * ω + χ
-		w = ξ(t) - (u_LI + p.current_background_power)
-        P_gen = (u_LI + p.current_background_power) * η_gen(t)
-		dχ = (- ω - K_I * χ) * T
-		# dϕ = ω
-        dω = (P_gen - total_flow(e_s, e_d)) * M # F = total_flow(e_s, e_d)
-        try
-            dx[1] = ω
-            dx[2] = dω
-			dx[3] = dχ
-			dx[4] = u_LI
-			dx[5] = w
-            return nothing
-        catch e
-            if typeof(e) === UndefVarError
-                throw(NodeDynamicsError("you need to provide $(e.var)"))
-            else
-                throw(e)
-            end
-        end
+		u_ILC = p.current_background_power
+		F = total_flow(e_s, e_d)
+		println("This is the net flow from node PV ", F)
+
+		dϕ = ω[1]
+		u_LI = - K_P * ω[1] + χ[1]
+		P_gen = (ξ(t) + (u_LI + u_ILC)) * η_gen(t)
+		w = 0
+		dω = (P_gen - F) * M_inv
+		dχ = (- ω[1] - K_I * χ[1]) * T_inv
+
+		dx[1] = dϕ
+		dx[2] = dω
+		dx[3] = dχ
+		dx[4] = u_LI
+		dx[5] = w
+
+		nothing
     end
-    ODEVertex(f! = rhs!, dim = 5, mass_matrix = [1, 1, 1, 1, 1], sym = [:ϕ, :ω, :χ, :integrated_LI, :integrated_w])
+    ODEVertex(f! = rhs!, dim = 5, mass_matrix = [1, 1, 1, 1, 0], sym = [:ϕ, :ω, :χ, :integrated_LI, :integrated_w])
 end
 
 struct Load <: AbstractNode
         ξ
         η_load
         LI
-        M
+        M_inv
 end
 
-Load(; ξ, η_load, LI, M) = Load(ξ, η_load, LI, M)
+Load(; ξ, η_load, LI, M_inv) = Load(ξ, η_load, LI, M_inv)
 function construct_vertex(par::Load)
 	ξ = par.ξ
     η_load = par.η_load
     K_P = par.LI.kp
 	K_I = par.LI.ki
-	T = par.LI.T
-    M = par.M
+	T_inv = par.LI.T_inv
+	M_inv = par.M_inv
 
 	function rhs!(dx, x, e_s, e_d, p, t)
-		ϕ = x[1]
-		ω = x[2]
-		χ = x[3]
-		integrated_LI = x[4]
-		integrated_w = x[5]
+		ϕ = @view x[1]
+		ω = @view x[2]
+		χ = @view x[3]
+		integrated_LI = @view x[4]
+		integrated_w = @view x[5]
 
-		u_LI = - K_P * ω + χ
-		w = ξ(t) - (u_LI + p.current_background_power)
-		P_load = (u_LI + p.current_background_power) / η_load(t)
-		dχ = (- ω - K_I * χ) * T
-		# dϕ = ω
-		dω = (- P_load - total_flow(e_s, e_d)) * M # F = total_flow(e_s, e_d)
-		try
-			dx[1] = ω
-			dx[2] = dω
-			dx[3] = dχ
-			dx[4] = u_LI
-			dx[5] = w
-			return nothing
-		catch e
-			if typeof(e) === UndefVarError
-				throw(NodeDynamicsError("you need to provide $(e.var)"))
-			else
-				throw(e)
-			end
-		end
+		u_ILC = p.current_background_power
+		F = total_flow(e_s, e_d)
+		println("This is the net flow from node load ", F)
+
+		dϕ = ω[1]
+		u_LI = - K_P * ω[1] + χ[1]
+		w = 0
+		P_load = (ξ(t) + (u_LI + u_ILC))/ η_load(t)
+		dω = (- P_load - F) * M_inv
+		dχ = (- ω[1] - K_I * χ[1]) * T_inv
+
+		dx[1] = dϕ
+		dx[2] = dω
+		dx[3] = dχ
+		dx[4] = u_LI
+		dx[5] = w
+
+		nothing
 	end
-	ODEVertex(f! = rhs!, dim = 5, mass_matrix = [1, 1, 1, 1, 1], sym = [:ϕ, :ω, :χ, :integrated_LI, :integrated_w])
+    ODEVertex(f! = rhs!, dim = 5, mass_matrix = [1, 1, 1, 1, 0], sym = [:ϕ, :ω, :χ, :integrated_LI, :integrated_w])
 end
 
 struct Slack <: AbstractNode
         ξ
         η_gen
         LI
-        M
+        M_inv
 end
 
-Slack(; ξ, η_gen, LI, M) = Slack(ξ, η_gen, LI, M)
+Slack(; ξ, η_gen, LI, M_inv) = Slack(ξ, η_gen, LI, M_inv)
 function construct_vertex(par::Slack)
     ξ = par.ξ
     η_gen = par.η_gen
     K_P = par.LI.kp
 	K_I = par.LI.ki
-	T = par.LI.T
-    M = par.M
+	T_inv = par.LI.T_inv
+	M_inv = par.M_inv
 
     function rhs!(dx, x, e_s, e_d, p, t)
-        ϕ = x[1]
-        ω = x[2]
-		χ = x[3]
-		integrated_LI = x[4]
-		integrated_w = x[5]
+		ϕ = @view x[1]
+		ω = @view x[2]
+		χ = @view x[3]
+		integrated_LI = @view x[4]
+		integrated_w = @view x[5]
 
-		u_LI = - K_P * ω + χ
-		w = ξ(t) - (u_LI + p.current_background_power)
-        P_gen = (u_LI + p.current_background_power) * η_gen(t)
-		dχ = (- ω - K_I * χ) * T
-		# dϕ = ω
-        dω = (P_gen - total_flow(e_s, e_d)) * M # F = total_flow(e_s, e_d)
-        try
-            dx[1] = ω
-            dx[2] = dω
-			dx[3] = dχ
-			dx[4] = u_LI
-			dx[5] = w
-            return nothing
-        catch e
-            if typeof(e) === UndefVarError
-                throw(NodeDynamicsError("you need to provide $(e.var)"))
-            else
-                throw(e)
-            end
-        end
+		u_ILC = p.current_background_power
+		F = total_flow(e_s, e_d)
+		println("This is the net flow from node slack ", F)
+
+		dϕ = ω[1]
+		u_LI = - K_P * ω[1] + χ[1]
+		w = 0
+		P_gen = (ξ(t) + u_LI + u_ILC) * η_gen(t)
+		dχ = (- ω[1] - K_I * χ[1]) * T_inv
+		dω = (P_gen - F) * M_inv
+
+		dx[1] = dϕ
+		dx[2] = dω
+		dx[3] = dχ
+		dx[4] = u_LI
+		dx[5] = w
+
+		nothing
     end
-    ODEVertex(f! = rhs!, dim = 5, mass_matrix = [1, 1, 1, 1, 1], sym = [:ϕ, :ω, :χ, :integrated_LI, :integrated_w])
+    ODEVertex(f! = rhs!, dim = 5, mass_matrix = [1, 1, 1, 1, 0], sym = [:ϕ, :ω, :χ, :integrated_LI, :integrated_w])
 end
