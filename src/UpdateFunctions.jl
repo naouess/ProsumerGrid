83f697c1610afb6fecc2c1b725228d8713479252
naouess <n.ouanes@posteo.de>
2020-08-17 13:01:30 +0200

    Corrected indexing error and added print-lines to help debug

--- src/UpdateFunctions.jl
+++ src/UpdateFunctions.jl
@@ -14,27 +14,30 @@ PeriodicCallback function acting on the `integrator` that is called every simula
 """
 function (hu::HourlyUpdate)(integrator)
 	# The current hour is extracted using the following line:
+	println("The time is currently ", integrator.t)
 	hour = mod(round(Int, integrator.t/3600.), 24) + 1
-	# Needed for indexing (one hour is from 0 to 1)
+	# getting the last hour of the day
 	last_hour = mod(hour-2, 24) + 1
-	println("I'm doing this")
+	# println("The hour is ", last_hour)
 
 	# the following lines: last hour letzte Stunde auf Integral der Stunde davor setzen
 	for j = 1:N
-		integrator.p[1][j].mismatch_yesterday[last_hour] = integrator.u[4*j]
-		integrator.u[4*j] = 0.
+		integrator.p[1][j].mismatch_yesterday[last_hour] = integrator.u[5*j-1]
+		# println(integrator.p[1][j].mismatch_yesterday[last_hour])
+		integrator.u[5*j-1] = 0.
 		integrator.p[1][j].current_background_power = integrator.p[1][j].daily_background_power[hour]
+		println("I'm doing this at time ", integrator.t, " for node ", j)
 	end
 	# Why is the abs. value needed ?
 	# integrator.u[power_abs_idx] .= 0.
 	nothing
 end
 
-function DailyUpdate_X(integrator)
+function DailyUpdate(integrator)
 	for j = 1:N
-		integrator.p[1][j].daily_background_power .= integrator.p[1][j].Q * (integrator.p[1][j].daily_background_power
-		.+ integrator.p[1][j].kappa * integrator.p[1][j].mismatch_yesterday)
-		println("And also this")
+		integrator.p[1][j].daily_background_power = integrator.p[1][j].Q * (integrator.p[1][j].daily_background_power
+		+ integrator.p[1][j].kappa * integrator.p[1][j].mismatch_yesterday)
+		println("And also this at time ", integrator.t, " for node ", j)
 	end
 	nothing
 end
