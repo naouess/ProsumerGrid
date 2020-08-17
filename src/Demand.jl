d726e786f0baa0e408a37148338ec9da20133a8f
naouess <n.ouanes@posteo.de>
2020-08-18 00:05:41 +0200

    Added real demand data

--- src/Demand.jl
+++ src/Demand.jl
@@ -1,4 +1,6 @@
 using Interpolations
+using JLD2, FileIO, GraphIO, CSV, DataFrames
+
 begin
 	# num_days = 20
 	# l_day = 24*3600
@@ -16,16 +18,36 @@ begin
 		dav.demand[index + 1,:]
 	end
 
-	# slowly increasing and decreasing amplitude - only working for <= 20 days now
-	demand_amp1 = demand_amp_var(repeat([80 80 80 10 10 10 40 40 40 40 40], outer=Int(N/2))') # random positive amp over days by 10%
-	demand_amp2 = demand_amp_var(repeat([10 10 10 80 80 80 40 40 40 40 40], outer=Int(N/2))') # random positive amp over days by 10%
-	demand_amp3 = demand_amp_var(repeat([60 60 60 60 10 10 10 40 40 40 40], outer=Int(N/2))') # random positive amp over days by 10%
-	demand_amp4 = demand_amp_var(repeat([30 30 30 30 10 10 10 80 80 80 80], outer=Int(N/2))') # random positive amp over days by 10%
-	demand_amp = t -> vcat(demand_amp1(t), demand_amp2(t))#, demand_amp3(t), demand_amp4(t))
+	# Real demand data:
+	dem_data = CSV.read("$dir/profil.csv")
 
-	periodic_demand =  t-> demand_amp(t)./100 .* sin(t*pi/(24*3600))^2
-	samples = 24*N
-	inter = interpolate([.2 * randn(N) for i in 1:(num_days * samples + 1)], BSpline(Linear()))
+	dem_data_week = CSV.read("$dir/profil_week.csv")
+
+	week_winter = t->dem_data_week[!,:Winterwoche][Int(floor(mod(t,24*3600*7) / 900)+1)]
+	week_summer = t->dem_data_week[!,:Sommerwoche][Int(floor(mod(t,24*3600*7) / 900)+1)]
+	week_between = t->dem_data_week[!,:Uebergangswoche][Int(floor(mod(t,24*3600*7) / 900)+1)]
+	week_G1 = t->dem_data_week[!,:WinterwocheG1][Int(floor(mod(t,24*3600*7) / 900)+1)]
+	week_G4 = t->dem_data_week[!,:WinterwocheG4][Int(floor(mod(t,24*3600*7) / 900)+1)]
+
+	week = t->vcat(week_winter(t), week_G1(t), week_G4(t), (week_winter(t) + week_G1(t) + week_G4(t))./3)
+
+
+	periodic_demand = t ->  week(t) ./100
+	samples = 24*60 #24*4
+	inter = interpolate([10. ./100 * randn(N) for i in 1:(num_days * samples + 1)], BSpline(Linear()))
 	residual_demand = t -> inter(1. + t / (24*3600) * samples) # 1. + is needed to avoid trying to access out of range
+
+    # Synthetic demand function:
+	# slowly increasing and decreasing amplitude - only working for <= 20 days now
+	# demand_amp1 = demand_amp_var(repeat([80 80 80 10 10 10 40 40 40 40 40], outer=Int(N/4))') # random positive amp over days by 10%
+	# demand_amp2 = demand_amp_var(repeat([10 10 10 80 80 80 40 40 40 40 40], outer=Int(N/4))') # random positive amp over days by 10%
+	# demand_amp3 = demand_amp_var(repeat([60 60 60 60 10 10 10 40 40 40 40], outer=Int(N/4))') # random positive amp over days by 10%
+	# demand_amp4 = demand_amp_var(repeat([30 30 30 30 10 10 10 80 80 80 80], outer=Int(N/4))') # random positive amp over days by 10%
+	# demand_amp = t -> vcat(demand_amp1(t), demand_amp2(t), demand_amp3(t), demand_amp4(t))
+	#
+	# periodic_demand =  t-> demand_amp(t)./100 .* sin(t*pi/(24*3600))^2
+	# samples = 24*N
+	# inter = interpolate([.2 * randn(N) for i in 1:(num_days * samples + 1)], BSpline(Linear()))
+	# residual_demand = t -> inter(1. + t / (24*3600) * samples) # 1. + is needed to avoid trying to access out of range
 end
 # plot(1:100000, t -> periodic_demand(t)[1]+residual_demand(t)[1])
