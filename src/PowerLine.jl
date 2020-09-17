6277b3a8e12c17a3c9476c5daf41f0c657683282
naouess <n.ouanes@posteo.de>
2020-09-18 00:40:10 +0200

    Make PowerLine mutable for some experimentations with callbacks

--- src/PowerLine.jl
+++ src/PowerLine.jl
@@ -1,19 +1,15 @@
 abstract type AbstractLine end
 
-struct PowerLine <: AbstractLine
+mutable struct PowerLine <: AbstractLine
     from
     to
     K
     PowerLine(; from, to, K) = new(from, to, K)
 end
 
-function construct_edge(par::PowerLine)
-    from = par.from
-    to = par.to
-    K = par.K
-    function edgefunction!(e, v_s, v_d, p, t)
-        e[1] = K * sin(v_s[1] - v_d[1])
-        nothing
-    end
-    return StaticEdge(f! = edgefunction!, dim = 1)
+function (edge::PowerLine)(e, v_s, v_d, p, t)
+    e[1] = edge.K * sin(v_s[1] - v_d[1])
+    nothing
 end
+
+# export PowerLine
