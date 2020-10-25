cbdb0d69b9971cefc54418ae3fdeb2a02b5c0ff6
naouess <n.ouanes@posteo.de>
2020-10-25 20:51:21 +0100

    Update real data files

--- src/MinigridDemand.jl
+++ src/MinigridDemand.jl
@@ -1,19 +1,13 @@
 using JLD2, FileIO, GraphIO, CSV, DataFrames
+using Statistics
+using StatsBase
 
 begin
 	## Generating demand function out of real minigrid data
-	dem_data_week = CSV.read("$dir/demand/Week_March02_March08_2020.csv")
+	dem_data_twoweeks = CSV.read("$dir/src/demand/TwoWeeks.csv")
 
-	load = t->dem_data_week[!,:load][Int(floor(mod(t,24*3600*7) / 900)+1)]
-	pv_infeed = t->dem_data_week[!,:pv_infeed][Int(floor(mod(t,24*3600*7) / 900)+1)]
+	load = t->dem_data_twoweeks[!,:load][Int(floor(mod(t,24*3600*num_days) / 900)+1)]
+	pv_infeed = t->dem_data_twoweeks[!,:pv_infeed][Int(floor(mod(t,24*3600*num_days) / 900)+1)]
 
-	periodic_demand = t -> vcat(load(t), pv_infeed(t)) ./ 30. # , week_G4(t), (week_winter(t) + week_G1(t) + week_G4(t))./3)
-
-	# For additional fluctuations:
-	# samples = 24*60
-	# inter = interpolate([10. ./100 * randn(2) for i in 1:(num_days * samples + 1)], BSpline(Linear()))
-	# residual_demand = t -> inter(1. + t / (24*3600) * samples)
-
-	demand = t -> periodic_demand(t) # + residual_demand(t)
-
+	demand_real = t -> vcat(load(t), pv_infeed(t)) ./ 50.
 end
